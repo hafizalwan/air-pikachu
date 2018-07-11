2ef9505327e2f2ade09151fa2e6d81f23fe5f246
Hafiz Alwan <hafizalwan@gmail.com>
2018-07-11 20:39:01 +0800

    Create fullname authentication validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :fullname, presence: true, lenght: {maximum: 50}
 end
